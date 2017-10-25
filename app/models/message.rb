87d960eb4ffd351ea96085d6e9aaa203ccafda81
Ha Dinh Nhan <hadinhnhan1102@gmail.com>
2017-10-25 23:02:50 +0700

    Fix bug không ghi người gửi

--- app/models/message.rb
+++ app/models/message.rb
@@ -2,7 +2,6 @@ class Message < ApplicationRecord
   PARAMS_COMPOSE = [:sender, :receiver, :subject, :body]
   EMAIL_REGEX = /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\z/i
 
-  validates :sender, format: {with: EMAIL_REGEX}, presence: true
   validates :receiver, format: {with: EMAIL_REGEX}, presence: true
 
 end
