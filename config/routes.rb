56453a931988b69b1d3c33bab786ba14a32e5abc
Ha Dinh Nhan <hadinhnhan1102@gmail.com>
2017-10-25 14:33:08 +0700

    Đặt root thành trang login

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
+
+  root :to => "sessions#new"
+
   get     "login", to: "sessions#new"
   post    "login", to: "sessions#create"
   delete  "logout"   => "sessions#destroy"
