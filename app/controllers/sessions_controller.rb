fa03620639733b2a69c52ee3ea7324549f58e199
Ha Dinh Nhan <hadinhnhan1102@gmail.com>
2017-10-25 15:06:33 +0700

    Fix bug: Chưa confirm mail đã đăng nhập được

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -7,9 +7,14 @@ class SessionsController < ApplicationController
   def create
     user = User.find_by email_address: params[:session][:email_address].to_s.downcase.gsub(/\s+/, '')
     if user && user.authenticate(params[:session][:password])
+      if user.confirmed_at==nil
+        flash[:warning] = "Please confirm your email first"
+        redirect_to login_path
+      else
       flash[:success] = "Login success"
       log_in user
       redirect_to user
+      end
     else
       flash[:danger] = "Invalid email/password combination"
       render :new
